a87c4da3fe7a4a5f86d91fe7cb109d3f26d9e35d
normal-hugh <HBSmith5000@gmail.com>
2023-09-01 17:45:20 -0700

    cleanup

--- bin/cdk-init.js
+++ bin/cdk-init.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { App } from 'aws-cdk-lib';
-import widget_service from '../lib/gameDataStack.js';
+import myStack from '../lib/gameDataStack.js';
 
 const app = new App();
-new widget_service.MyStack(app, 'GameDataStack');
+new myStack.MyStack(app, 'GameDataStack');
