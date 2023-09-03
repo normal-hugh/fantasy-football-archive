70f2cfc48dc29830d366ce86e0e9489bce8787d8
normal-hugh <HBSmith5000@gmail.com>
2023-09-02 18:57:26 -0700

    week and year, still broken for idp and res somehow

--- resources/gameDataLambda.js
+++ resources/gameDataLambda.js
@@ -24,6 +24,11 @@ export const handler = async (event) => {
     const getObjectCommand = new GetObjectCommand(params);
     const response = await s3Client.send(getObjectCommand);
 
+    const weekYear = fileName.split('.csv')[0].split('W')
+    const week = weekYear[1]
+    const year = weekYear[0]
+    console.log(`parsing game results for week ${week} of ${year}`)
+
     const streamToString = (stream) => {
       return new Promise((resolve, reject) => {
           const chunks = [];
@@ -38,59 +43,40 @@ export const handler = async (event) => {
     console.log(`Object content: ${typeof objectContent}`);
 
     const stringArray = objectContent.split(',');
-    console.log(`
-      0: ${stringArray[0]}\n
-      1: ${stringArray[1]}\n
-      2: ${stringArray[2]}\n
-      3: ${stringArray[3]}\n
-      4: ${stringArray[4]}\n
-      5: ${stringArray[5]}\n
-      6: ${stringArray[6]}\n
-      7: ${stringArray[7]}\n
-      8: ${stringArray[8]}\n
-      9: ${stringArray[9]}\n
-      10: ${stringArray[10]}\n
-      11: ${stringArray[11]}\n
-      12: ${stringArray[12]}\n
-      13: ${stringArray[13]}\n
-      14: ${stringArray[14]}\n
-      15: ${stringArray[15]}\n
-      16: ${stringArray[16]}\n
-      17: ${stringArray[17]}\n
-      18: ${stringArray[18]}\n
-      19: ${stringArray[19]}\n
-      20: ${stringArray[20]}\n
-    `)
     const gameLog = (strArr, startingEntry = 0, game = {}, gameNumber = 1) => {
       let entry = startingEntry; // column counter
       let teamA = {};
 
+      const adjTeamName = entry !== 0
+        ? strArr[entry]
+        : strArr[entry].split('\r')[1].split('\n')[1];
       teamA.name = {
-        value: strArr[entry]
+        value: adjTeamName
       };
       entry += 8;
 
       // team A starting lineup 
       while (!strArr[entry].includes('Points:')) {
-        teamA[`teamA${strArr[entry].split('\n')[1]}`] ={
+        const position = strArr[entry].split('\n')[1].replace('W/R', 'Flex')
+        const adjPosition = teamA[`teamA${position}`] ? `teamA${position}2` : `teamA${position}`;
+        teamA[adjPosition] = {
           player: strArr[entry + 1],
-          points: parseInt(strArr[entry + 2].split('\n')[0].split('\r')[0])
+          points: parseFloat(strArr[entry + 2].split('\n')[0].split('\r')[0])
         }
         entry += 2
         if (teamA.length > 1000) {
           break;
         }
       }
-      console.log(`teamA: ${JSON.stringify(teamA)}`);
 
       entry += 8;
 
       // team A bench 
       let teamABenchCount = 1
       while (!strArr[entry].includes('Points:')) {
-        teamA[`${strArr[entry].split('\r')[1].split('\n')[1]}${teamABenchCount}`] = {
+        teamA[`teamA${strArr[entry].split('\r')[1].split('\n')[1]}${teamABenchCount}`] = {
           player: strArr[entry + 1],
-          points: parseInt(strArr[entry + 2].split('\n')[0].split('\r')[0])
+          points: parseFloat(strArr[entry + 2].split('\n')[0].split('\r')[0])
         }
         entry += 2
         teamABenchCount += 1;
@@ -98,7 +84,6 @@ export const handler = async (event) => {
           break;
         }
       }
-      console.log(`teamA with bench: ${JSON.stringify(teamA)}`);
       entry += 2;
 
       let teamB = {};
@@ -110,25 +95,26 @@ export const handler = async (event) => {
 
       // team B starting lineup 
       while (!strArr[entry].includes('Points:')) {
-        teamB[`teamB${strArr[entry].split('\n')[1]}`] ={
+        const position = strArr[entry].split('\n')[1].replace('W/R', 'Flex')
+        const adjPosition = teamB[`teamB${position}`] ? `teamB${position}2` : `teamB${position}`;
+        teamB[adjPosition] = {
           player: strArr[entry + 1],
-          points: parseInt(strArr[entry + 2].split('\n')[0].split('\r')[0])
+          points: parseFloat(strArr[entry + 2].split('\n')[0].split('\r')[0])
         }
         entry += 2
         if (teamB.length > 1000) {
           break;
         }
       }
-      console.log(`teamB: ${JSON.stringify(teamB)}`);
 
       entry += 8;
 
       // team B bench 
       let teamBBenchCount = 1
       while (!strArr[entry].includes('Points:')) {
-        teamB[`${strArr[entry].split('\r')[1].split('\n')[1]}${teamBBenchCount}`] = {
+        teamB[`teamB${strArr[entry].split('\r')[1].split('\n')[1]}${teamBBenchCount}`] = {
           player: strArr[entry + 1],
-          points: parseInt(strArr[entry + 2].split('\n')[0].split('\r')[0])
+          points: parseFloat(strArr[entry + 2].split('\n')[0].split('\r')[0])
         }
         entry += 2
         teamBBenchCount += 1;
@@ -136,7 +122,6 @@ export const handler = async (event) => {
           break;
         }
       }
-      console.log(`teamB with bench: ${JSON.stringify(teamB)}`);
       entry += 2;
 
       const fullGameLog = {
